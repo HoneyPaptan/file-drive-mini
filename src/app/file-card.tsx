bd2fd6ec7b1555aef2f0901a0a48608d9e8f1f09
Honey Paptan <hpaptan@gmail.com>
2024-06-24 01:52:43 +0530

    add ability to delete files

---
+++ src/app/file-card.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Doc } from "../../convex/_generated/dataModel";
+import { Button } from "@/components/ui/button";
+import { DeleteIcon, MoreVertical, TrashIcon } from "lucide-react";
+import { useState } from "react";
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import { toast } from "sonner";
+
+function FileCardActions({ file }: { file: Doc<"files"> }) {
+  const deleteFile = useMutation(api.files.deleteFile);
+  const [isConfirmOpen, setIsConfirmOpen] = useState(false);
+  return (
+    <>
+      <AlertDialog open={isConfirmOpen} onOpenChange={setIsConfirmOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={() => {
+                // todo: actually delete the file
+                const promise = deleteFile({
+                  fileId: file._id,
+                });
+                toast.promise(promise, {
+                  loading: "Deleting  your file",
+                  success: "File Deleted Successfully",
+                  error: "Error while Deleting. Please try again",
+                });
+              }}
+            >
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger className="focus:ring-0 focus:outline-none">
+          <MoreVertical />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem
+            className="flex gap-1 text-red-600 items-center"
+            onClick={() => setIsConfirmOpen(true)}
+          >
+            <TrashIcon className="w-4 h-4" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+}
+
+export function FileCard({ file }: { file: Doc<"files"> }) {
+  return (
+    <Card>
+      <CardHeader className="relative">
+        <CardTitle>{file.name}</CardTitle>
+        <div className="absolute top-2 right-2">
+          <FileCardActions file={file} />
+        </div>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <Button>Download</Button>
+      </CardFooter>
+    </Card>
+  );
+}
---
+++ src/app/upload-button.tsx
@@ -0,0 +1,155 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useOrganization, useUser } from "@clerk/nextjs";
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { useState } from "react";
+import { toast } from "sonner";
+import { Loader2 } from "lucide-react";
+
+const formSchema = z.object({
+  title: z.string().min(1).max(200),
+  file: z
+    .custom<FileList>((val) => val instanceof FileList, "Required")
+    .refine((files) => files.length > 0, `Required`),
+});
+
+export function UploadButton() {
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl);
+  const user = useUser();
+  const organization = useOrganization();
+  const createFile = useMutation(api.files.createFile);
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      file: undefined,
+    },
+  });
+
+  const fileRef = form.register("file");
+  // 2. Define a submit handler.
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    if (!orgId) {
+      return;
+    }
+    // Step 1: Get a short-lived upload URL
+    const postUrl = await generateUploadUrl();
+    // Step 2: POST the file to the URL
+    const result = await fetch(postUrl, {
+      method: "POST",
+      headers: { "Content-Type": values.file[0]!.type },
+      body: values.file[0],
+    });
+    const { storageId } = await result.json();
+
+    const promise = createFile({
+      name: values.title,
+      fileId: storageId,
+      orgId: orgId,
+    });
+    toast.promise(promise, {
+      loading: "Uploading your file",
+      success: "File Uploaded Successfully",
+      error: "Error while uploading. Please try again",
+    });
+    form.reset();
+    setIsDialogOpen(false);
+  }
+  let orgId: string | undefined = undefined;
+  // logic for organization id and if not in an organization then use the user id
+  // state for opening and closing dialog
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  if (organization.isLoaded && user.isLoaded) {
+    // when both of these are loaded then set the orgId to one which is present
+    orgId = organization.organization?.id ?? user.user?.id;
+  }
+
+  return (
+    <Dialog
+      open={isDialogOpen}
+      onOpenChange={(isOpen) => {
+        setIsDialogOpen(isOpen);
+        form.reset();
+      }}
+    >
+      <DialogTrigger asChild>
+        <Button onClick={() => {}}>Upload File</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="mb-8">
+            Upload your file to File-Drive-Mini
+          </DialogTitle>
+
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Title</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter the title" {...field} />
+                    </FormControl>
+
+                    <div>
+                      <FormMessage />
+                    </div>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="file"
+                render={() => (
+                  <FormItem>
+                    <FormLabel>File</FormLabel>
+                    <FormControl>
+                      <Input type="file" {...fileRef} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                disabled={form.formState.isSubmitting}
+                className="flex gap-1"
+              >
+                {form.formState.isSubmitting && (
+                  <Loader2 className=" h-4 w-4 animate-spin" />
+                )}
+                Submit
+              </Button>
+            </form>
+          </Form>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
